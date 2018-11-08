b7710c67872284852640117cf7c81059f13c2cc4
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2018-11-08 18:32:44 +0000

    Add simple uninstrumented data store to test app

---
+++ EventScope.Test/DataStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventScope.Test
+{
+    internal class DataStore
+    {
+        private readonly Random _random;
+
+        public DataStore()
+        {
+            _random = new Random();
+        }
+
+        public async Task<byte[]> ReadRandomBytesAsync(int size)
+        {
+            return await Task.WhenAll(new object[size].Select(_ => ReadRandomByteAsync()));
+        }
+
+        private async Task<byte> ReadRandomByteAsync()
+        {
+            var buffer = new byte[1];
+            _random.NextBytes(buffer);
+            await Task.Delay(TimeSpan.FromSeconds(_random.NextDouble())).ConfigureAwait(false);
+            return buffer[0];
+        }
+    }
+}
--- EventScope.Test/Program.cs
+++ EventScope.Test/Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Serilog;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace EventScope.Test
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            var dataStore = new DataStore();
+
+            for (var index = 0; index < 8; index++)
+            {
+                var buffer = await dataStore.ReadRandomBytesAsync(16).ConfigureAwait(false);
+                var hexString = string.Concat(buffer.Select(b => $"{b:X2}"));
+                Log.Information("Read random bytes from data store: {BytesHex}", hexString);
+            }
+
+            Log.Information("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
