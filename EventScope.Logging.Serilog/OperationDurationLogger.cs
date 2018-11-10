7732bd3e07b0079886ca3aaf439382bfc5e62a27
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2018-11-10 17:25:46 +0000

    Update log message template

--- EventScope.Logging.Serilog/OperationDurationLogger.cs
+++ EventScope.Logging.Serilog/OperationDurationLogger.cs
@@ -20,8 +20,8 @@ namespace EventScope.Logging.Serilog
         {
             _logger.Write(
                 _logEventLevel,
-                "{OperationName} completed in {OperationDurationSeconds}s",
-                _operationName, eventArgs.OperationDuration.TotalSeconds);
+                "Took {OperationDurationSeconds}s to {OperationName}",
+                eventArgs.OperationDuration.TotalSeconds, _operationName);
         }
     }
 }
