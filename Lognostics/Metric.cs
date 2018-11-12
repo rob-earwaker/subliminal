1ba87e91f523d894308a0c14686deb9871850500
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2018-11-12 18:59:00 +0000

    Add metric type

---
+++ Lognostics/Metric.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lognostics
+{
+    public class Metric<TValue>
+    {
+        public event EventHandler<MetricSampledEventArgs<TValue>> Sampled;
+
+        public void LogValue(TValue value)
+        {
+            Sampled?.Invoke(this, new MetricSampledEventArgs<TValue>(value));
+        }
+    }
+}
