80048bf82e485262712c563a968b66970d2b8396
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2018-11-13 18:58:27 +0000

    Implement event counter

---
+++ Lognostics/EventCounter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lognostics
+{
+    public class EventCounter<TEventArgs> : IEventHandler<TEventArgs> where TEventArgs : EventArgs
+    {
+        private int _eventCount;
+        private readonly object _eventCountLock;
+
+        public EventCounter()
+        {
+            _eventCount = 0;
+            _eventCountLock = new object();
+        }
+
+        public void HandleEvent(object sender, TEventArgs eventArgs)
+        {
+            lock (_eventCountLock)
+            {
+                _eventCount++;
+            }
+        }
+    }
+}
