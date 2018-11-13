5d3148752e027c6764e9557a1315929a28002fb5
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2018-11-13 19:02:24 +0000

    Add aggregate event handler

---
+++ Lognostics/AggregateEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lognostics
+{
+    public class AggregateEventHandler<TEventArgs> : IEventHandler<TEventArgs> where TEventArgs : EventArgs
+    {
+        private readonly IEventHandler<TEventArgs>[] _eventHandlers;
+
+        public AggregateEventHandler(params IEventHandler<TEventArgs>[] eventHandlers)
+        {
+            _eventHandlers = eventHandlers;
+        }
+
+        public void HandleEvent(object sender, TEventArgs eventArgs)
+        {
+            foreach (var eventHandler in _eventHandlers)
+            {
+                eventHandler?.HandleEvent(sender, eventArgs);
+            }
+        }
+    }
+}
