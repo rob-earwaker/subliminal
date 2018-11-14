18f9467bc6845def6b3ea65412d4b24244daada6
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2018-11-14 22:18:29 +0000

    Fix build number parsing

--- Build.ps1
+++ Build.ps1
@@ -4,6 +4,5 @@ dotnet clean -c $buildConfiguration .
 dotnet build -c $buildConfiguration .
 dotnet test -c $buildConfiguration .\Lognostics.Tests
 $versionSuffix =
-    If ($null -eq $env:APPVEYOR_BUILD_NUMBER) {"local"}
-    Else {[System]::Int32.Parse($env:APPVEYOR_BUILD_NUMBER).ToString("0000")}
+    If ($null -eq $env:APPVEYOR_BUILD_NUMBER) {"local"} Else {[System.Int32]::Parse($env:APPVEYOR_BUILD_NUMBER).ToString("0000")}
 dotnet pack -c $buildConfiguration --version-suffix $versionSuffix -o ..\artifacts .\Lognostics
