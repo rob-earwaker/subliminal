d0b0ad6d0f363eef271fbf9865cae75bf4b5c095
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2018-11-14 22:11:22 +0000

    Add build script

---
+++ Build.ps1
@@ -0,0 +1,9 @@
+Remove-Item -Path .\artifacts -ErrorAction Ignore
+$buildConfiguration = "Release"
+dotnet clean -c $buildConfiguration .
+dotnet build -c $buildConfiguration .
+dotnet test -c $buildConfiguration .\Lognostics.Tests
+$versionSuffix =
+    If ($null -eq $env:APPVEYOR_BUILD_NUMBER) {"local"}
+    Else {[System]::Int32.Parse($env:APPVEYOR_BUILD_NUMBER).ToString("0000")}
+dotnet pack -c $buildConfiguration --version-suffix $versionSuffix -o ..\artifacts .\Lognostics
