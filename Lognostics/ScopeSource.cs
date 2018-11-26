479249dc86e700ea96909c4c67b870bed7aa7fbe
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2018-11-26 21:40:48 +0000

    Add scope source

---
+++ Lognostics/ScopeSource.cs
@@ -0,0 +1,30 @@
+﻿using Lognostics.Events;
+using System.Collections.Generic;
+
+namespace Lognostics
+{
+    public class ScopeSource : IScopeSource
+    {
+        private readonly ConcurrentHashSet<IScope> _activeScopes;
+
+        public ScopeSource()
+        {
+            _activeScopes = new ConcurrentHashSet<IScope>();
+        }
+
+        public ICollection<IScope> ActiveScopes => _activeScopes.Snapshot();
+
+        public IScope StartNew()
+        {
+            var scope = Scope.StartNew();
+            scope.Ended += ScopeEndedHandler;
+            _activeScopes.Add(scope);
+            return scope;
+        }
+
+        private void ScopeEndedHandler(object sender, ScopeEnded eventArgs)
+        {
+            eventArgs.Scope.Ended -= ScopeEndedHandler;
+        }
+    }
+}
