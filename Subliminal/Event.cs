6dd923cc2c0fb2f6c613caa04da06deffe814f08
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2018-12-05 21:04:54 +0000

    Add event type

---
+++ Subliminal/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Subliminal
+{
+    public class Event<TEventArgs> where TEventArgs : EventArgs
+    {
+        public event EventHandler<TEventArgs> Occured;
+
+        public void LogOccurrence(TEventArgs eventArgs)
+        {
+            Occured?.Invoke(this, eventArgs);
+        }
+    }
+}
