d4399cf09c90dfac27287921aa0ddfec31c2df8e
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2019-02-26 19:28:44 +0000

    Change method order

--- Subliminal/IObservableExtensions.cs
+++ Subliminal/IObservableExtensions.cs
@@ -7,14 +7,14 @@ namespace Subliminal
 {
     public static class IObservableExtensions
     {
-        public static IGauge<TValue> AsGauge<TValue>(this IObservable<TValue> source)
-        {
-            return Gauge<TValue>.FromSource(source);
-        }
-
         public static IGauge<TValue> AsGauge<TValue>(this IObservable<GaugeSampled<TValue>> source)
         {
             return new Gauge<TValue>(source);
+        }
+
+        public static IGauge<TValue> AsGauge<TValue>(this IObservable<TValue> source)
+        {
+            return Gauge<TValue>.FromSource(source);
         }
 
         public static IObservable<IList<TSource>> Buffer<TSource>(this IObservable<TSource> source, Operation operation)
