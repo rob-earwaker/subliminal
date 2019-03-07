638a3f24662bc100981928eee4e4b97fe5debf5d
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2019-03-07 21:57:14 +0000

    Add Timer.Time methods

--- Subliminal/Timer.cs
+++ Subliminal/Timer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
 
 namespace Subliminal
 {
@@ -29,6 +30,58 @@ namespace Subliminal
         public IDisposable Subscribe(IObserver<TimeSpan> observer)
         {
             return _timer.Subscribe(observer);
+        }
+
+        public void Time(Action<RunningTimer> operation)
+        {
+            using (var timer = StartNew())
+            {
+                operation(timer);
+            }
+        }
+
+        public void Time(Action operation)
+        {
+            Time(_ => operation());
+        }
+
+        public TResult Time<TResult>(Func<RunningTimer, TResult> operation)
+        {
+            using (var timer = StartNew())
+            {
+                return operation(timer);
+            }
+        }
+
+        public TResult Time<TResult>(Func<TResult> operation)
+        {
+            return Time(_ => operation());
+        }
+
+        public async Task TimeAsync(Func<RunningTimer, Task> operation)
+        {
+            using (var timer = StartNew())
+            {
+                await operation(timer);
+            }
+        }
+
+        public Task TimeAsync<TResult>(Func<Task> operation)
+        {
+            return TimeAsync(_ => operation());
+        }
+
+        public async Task<TResult> TimeAsync<TResult>(Func<RunningTimer, Task<TResult>> operation)
+        {
+            using (var timer = StartNew())
+            {
+                return await operation(timer);
+            }
+        }
+
+        public Task<TResult> TimeAsync<TResult>(Func<Task<TResult>> operation)
+        {
+            return TimeAsync(_ => operation());
         }
     }
 }
