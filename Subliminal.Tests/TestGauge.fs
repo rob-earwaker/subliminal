81d7df051d9937a9fccaa3fcee2130bcd8a1ce0a
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2019-08-03 21:47:49 +0100

    Add gauge test

---
+++ Subliminal.Tests/TestGauge.fs
@@ -0,0 +1,15 @@
+﻿module Subliminal.Tests.TestGauge
+
+open FsCheck.Xunit
+open Subliminal
+open Swensen.Unquote
+
+[<Property>]
+let ``emits recorded values`` (value1: obj) (value2: obj) =
+    let gauge = Gauge<obj>()
+    let observer = TestObserver()
+    use subscription = gauge.Subscribe(observer)
+    gauge.LogValue(value1)
+    gauge.LogValue(value2)
+    test <@ observer.ObservedValues = [ value1; value2 ] @>
+    test <@ not observer.ObservableCompleted @>
