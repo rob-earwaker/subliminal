fb3f83b591fb948b87e0a9da7f1e6a7006fd228a
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2019-08-07 22:24:11 +0100

    Update build script to pass version numbers

--- Build.ps1
+++ Build.ps1
@@ -1,9 +1,31 @@
-Remove-Item -Path .\artifacts -Recurse -ErrorAction Ignore
-$buildConfiguration = "Release"
-dotnet clean -c $buildConfiguration .
-dotnet build -c $buildConfiguration .
-dotnet test -c $buildConfiguration .\Subliminal.Tests
+$majorVersionNumber = "1"
+$versionPrefix = "$majorVersionNumber.0.0"
 $versionSuffix =
     If ($null -eq $env:APPVEYOR_BUILD_NUMBER) {"local"}
     Else {[string]::Format("build-{0:D4}", [System.Int32]::Parse($env:APPVEYOR_BUILD_NUMBER))}
-dotnet pack -c $buildConfiguration --version-suffix $versionSuffix -o ..\artifacts .\Subliminal
+$version = "$versionPrefix-$versionSuffix"
+$assemblyVersion = "$majorVersionNumber.0.0.0"
+$fileVersion =
+    If ($null -eq $env:APPVEYOR_BUILD_NUMBER) {"$versionPrefix.0"}
+    Else {[string]::Format("$versionPrefix.{0:D4}", [System.Int32]::Parse($env:APPVEYOR_BUILD_NUMBER))}
+$informationalVersion =
+    If ($null -eq $env:APPVEYOR_REPO_COMMIT) {"$version"}
+    Else {[string]::Format("$version+{0}", $env:APPVEYOR_REPO_COMMIT.Substring(0, 7))}
+
+Write-Output "Version: $version"
+Write-Output "AssemblyVersion: $assemblyVersion"
+Write-Output "FileVersion: $fileVersion"
+Write-Output "InformationalVersion: $informationalVersion"
+
+Remove-Item -Path .\artifacts -Recurse -ErrorAction Ignore
+
+$buildConfiguration = "Release"
+dotnet clean . -c $buildConfiguration
+dotnet build . -c $buildConfiguration `
+    -p:Version=$version `
+    -p:AssemblyVersion=$assemblyVersion `
+    -p:FileVersion=$fileVersion `
+    -p:InformationalVersion=$informationalVersion
+dotnet test .\Subliminal.Tests -c $buildConfiguration --no-build
+dotnet pack .\Subliminal -c $buildConfiguration --no-build -o ..\artifacts `
+    -p:PackageVersion=$version
