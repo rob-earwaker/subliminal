daeeaae66733b4558ee81851f56b00e18364e457
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2019-08-13 21:51:50 +0100

    Rename sample app

--- Subliminal.SampleApp.Serilog/DataStore.cs
+++ Subliminal.SampleApp.Serilog/DataStore.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Subliminal.Serilog.TestApp
+namespace Subliminal.SampleApp.Serilog
 {
     internal class DataStore
     {
--- Subliminal.SampleApp.Serilog/Program.cs
+++ Subliminal.SampleApp.Serilog/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 
-namespace Subliminal.Serilog.TestApp
+namespace Subliminal.SampleApp.Serilog
 {
     internal class Program
     {
