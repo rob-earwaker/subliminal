d1b27cf25c55f05031ac8429e3ca7fac7ab7b307
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2019-08-17 21:22:01 +0100

    Add event tests

---
+++ Subliminal.Tests/TestEvent.fs
@@ -0,0 +1,32 @@
+﻿module Subliminal.Tests.``Test Event``
+
+open FsCheck.Xunit
+open Subliminal
+open Swensen.Unquote
+open System.Reactive
+
+[<Property>]
+let ``emits unit when raised`` () =
+    let event = Event()
+    let observer = TestObserver()
+    use subscription = event.Subscribe(observer)
+    event.Raise()
+    test <@ observer.ObservedValues = [ Unit.Default ] @>
+
+[<Property>]
+let ``completes after being raised`` () =
+    let event = Event()
+    let observer = TestObserver()
+    use subscription = event.Subscribe(observer)
+    event.Raise()
+    test <@ observer.ObservedValues = [ Unit.Default ] @>
+    test <@ observer.ObservableCompleted @>
+
+[<Property>]
+let ``only emits one value`` () =
+    let event = Event()
+    let observer = TestObserver()
+    use subscription = event.Subscribe(observer)
+    event.Raise()
+    event.Raise()
+    test <@ observer.ObservedValues = [ Unit.Default ] @>
