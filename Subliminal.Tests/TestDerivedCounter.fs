2761a2a2205ef97876cdd3d7710c344d1695cd20
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2019-08-17 20:54:35 +0100

    Add tests for other derived types

---
+++ Subliminal.Tests/TestDerivedCounter.fs
@@ -0,0 +1,81 @@
+﻿module Subliminal.Tests.TestDerivedCounter
+
+open FsCheck
+open FsCheck.Xunit
+open Subliminal
+open Swensen.Unquote
+open System
+open System.Reactive
+open System.Reactive.Linq
+open System.Reactive.Subjects
+
+type DerivedCounterFactory =
+    DerivedCounterFactory of deriveCounter:(IObservable<obj> -> ICounter<obj>) with
+    static member Arb =
+        [ fun observable -> DerivedCounter<obj>.FromObservable(observable) :> ICounter<obj>
+          fun observable -> observable.AsCounter<obj>() ]
+        |> Gen.elements
+        |> Gen.map DerivedCounterFactory
+        |> Arb.fromGen
+
+[<Property>]
+let ``emits increments`` (increment1: obj) (increment2: obj) =
+    let test (DerivedCounterFactory deriveCounter) =
+        use subject = new Subject<obj>()
+        let counter = deriveCounter subject
+        let observer = TestObserver()
+        use subscription = counter.Subscribe(observer)
+        subject.OnNext(increment1)
+        subject.OnNext(increment2)
+        test <@ observer.ObservedValues = [ increment1; increment2 ] @>
+    Prop.forAll DerivedCounterFactory.Arb test
+    
+[<Property>]
+let ``observers receive the same increments`` () =
+    let test (DerivedCounterFactory deriveCounter) =
+        // Create an observable that emits two values after a
+        // subject is completed. This is a cold observable and
+        // will emit different values to each observer.
+        use subject = new Subject<obj>()
+        let observable =
+            Observable.Concat(
+                subject,
+                Observable.Return(Unit.Default).Select(fun _ -> obj()),
+                Observable.Return(Unit.Default).Select(fun _ -> obj()))
+        let counter = deriveCounter observable
+        let observer1 = TestObserver()
+        let observer2 = TestObserver()
+        use subscription1 = counter.Subscribe(observer1)
+        use subscription2 = counter.Subscribe(observer2)
+        // Complete the subject to emit the values.
+        subject.OnCompleted()
+        // Both observers should have received the same values if the
+        // cold observable was converted into a hot observable.
+        test <@ observer1.ObservedValues.Length = 2 @>
+        test <@ observer2.ObservedValues.Length = 2 @>
+        test <@ observer1.ObservedValues = observer2.ObservedValues @>
+    Prop.forAll DerivedCounterFactory.Arb test
+    
+[<Property>]
+let ``starts emitting increments immediately`` (increment1: obj) (increment2: obj) =
+    let test (DerivedCounterFactory deriveCounter) =
+        // Create an observable that emits one value immediately
+        // and a second value when a subject is completed.
+        use subject = new Subject<obj>()
+        let observable =
+            Observable.Concat(
+                Observable.Return(increment1),
+                subject,
+                Observable.Return(increment2))
+        let counter = deriveCounter observable
+        let observer = TestObserver()
+        use subscription = counter.Subscribe(observer)
+        // The first value should have been emitted before the
+        // observer was subscribed if the derived observable
+        // started emitting values immediately.
+        test <@ observer.ObservedValues = [] @>
+        // Complete the subject to emit the second value.
+        subject.OnCompleted()
+        // The only observed value should be the second value.
+        test <@ observer.ObservedValues = [ increment2 ] @>
+    Prop.forAll DerivedCounterFactory.Arb test
---
+++ Subliminal.Tests/TestDerivedEventLog.fs
@@ -0,0 +1,81 @@
+﻿module Subliminal.Tests.TestDerivedEventLog
+
+open FsCheck
+open FsCheck.Xunit
+open Subliminal
+open Swensen.Unquote
+open System
+open System.Reactive
+open System.Reactive.Linq
+open System.Reactive.Subjects
+
+type DerivedEventLogFactory =
+    DerivedEventLogFactory of deriveEventLog:(IObservable<obj> -> IEventLog<obj>) with
+    static member Arb =
+        [ fun observable -> DerivedEventLog<obj>.FromObservable(observable) :> IEventLog<obj>
+          fun observable -> observable.AsEventLog<obj>() ]
+        |> Gen.elements
+        |> Gen.map DerivedEventLogFactory
+        |> Arb.fromGen
+
+[<Property>]
+let ``emits events`` (event1: obj) (event2: obj) =
+    let test (DerivedEventLogFactory deriveEventLog) =
+        use subject = new Subject<obj>()
+        let eventLog = deriveEventLog subject
+        let observer = TestObserver()
+        use subscription = eventLog.Subscribe(observer)
+        subject.OnNext(event1)
+        subject.OnNext(event2)
+        test <@ observer.ObservedValues = [ event1; event2 ] @>
+    Prop.forAll DerivedEventLogFactory.Arb test
+    
+[<Property>]
+let ``observers receive the same events`` () =
+    let test (DerivedEventLogFactory deriveEventLog) =
+        // Create an observable that emits two values after a
+        // subject is completed. This is a cold observable and
+        // will emit different values to each observer.
+        use subject = new Subject<obj>()
+        let observable =
+            Observable.Concat(
+                subject,
+                Observable.Return(Unit.Default).Select(fun _ -> obj()),
+                Observable.Return(Unit.Default).Select(fun _ -> obj()))
+        let eventLog = deriveEventLog observable
+        let observer1 = TestObserver()
+        let observer2 = TestObserver()
+        use subscription1 = eventLog.Subscribe(observer1)
+        use subscription2 = eventLog.Subscribe(observer2)
+        // Complete the subject to emit the values.
+        subject.OnCompleted()
+        // Both observers should have received the same values if the
+        // cold observable was converted into a hot observable.
+        test <@ observer1.ObservedValues.Length = 2 @>
+        test <@ observer2.ObservedValues.Length = 2 @>
+        test <@ observer1.ObservedValues = observer2.ObservedValues @>
+    Prop.forAll DerivedEventLogFactory.Arb test
+    
+[<Property>]
+let ``starts emitting events immediately`` (event1: obj) (event2: obj) =
+    let test (DerivedEventLogFactory deriveEventLog) =
+        // Create an observable that emits one value immediately
+        // and a second value when a subject is completed.
+        use subject = new Subject<obj>()
+        let observable =
+            Observable.Concat(
+                Observable.Return(event1),
+                subject,
+                Observable.Return(event2))
+        let eventLog = deriveEventLog observable
+        let observer = TestObserver()
+        use subscription = eventLog.Subscribe(observer)
+        // The first value should have been emitted before the
+        // observer was subscribed if the derived observable
+        // started emitting values immediately.
+        test <@ observer.ObservedValues = [] @>
+        // Complete the subject to emit the second value.
+        subject.OnCompleted()
+        // The only observed value should be the second value.
+        test <@ observer.ObservedValues = [ event2 ] @>
+    Prop.forAll DerivedEventLogFactory.Arb test
