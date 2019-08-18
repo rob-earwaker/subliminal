4838cbd70d80673b6cdc9b03b93b57bf949e2135
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2019-08-18 15:49:45 +0100

    Add tests for operation without context

--- Subliminal.Tests/TestOperation.fs
+++ Subliminal.Tests/TestOperation.fs
@@ -251,3 +251,208 @@ module ``Test Operation<TContext>`` =
         test <@ observer.ObservedValues.[0].Duration > TimeSpan.Zero @>
         test <@ not observer.ObservableCompleted @>
     
+module ``Test Operation`` =
+    [<Property>]
+    let ``ids are unique`` () =
+        let operation = Operation()
+        let observer = TestObserver()
+        use subscription = operation.Started.Subscribe(observer)
+        use timer1 = operation.StartNewTimer()
+        use timer2 = operation.StartNewTimer()
+        test <@ observer.ObservedValues.Length = 2 @>
+        test <@ observer.ObservedValues.[0].OperationId <> observer.ObservedValues.[1].OperationId @>
+        test <@ not observer.ObservableCompleted @>
+        
+    [<Property>]
+    let ``id in canceled operation matches started operation`` () =
+        let operation = Operation()
+        let startedObserver = TestObserver()
+        let canceledObserver = TestObserver()
+        use startedSubscription = operation.Started.Subscribe(startedObserver)
+        use canceledSubscription = operation.Canceled.Subscribe(canceledObserver)
+        use timer = operation.StartNewTimer()
+        timer.Cancel()
+        test <@ startedObserver.ObservedValues.Length = 1 @>
+        test <@ canceledObserver.ObservedValues.Length = 1 @>
+        test <@ startedObserver.ObservedValues.[0].OperationId = canceledObserver.ObservedValues.[0].OperationId @>
+        
+    [<Property>]
+    let ``id in completed operation matches started operation`` () =
+        let operation = Operation()
+        let startedObserver = TestObserver()
+        let completedObserver = TestObserver()
+        use startedSubscription = operation.Started.Subscribe(startedObserver)
+        use completedSubscription = operation.Completed.Subscribe(completedObserver)
+        use timer = operation.StartNewTimer()
+        timer.Stop()
+        test <@ startedObserver.ObservedValues.Length = 1 @>
+        test <@ completedObserver.ObservedValues.Length = 1 @>
+        test <@ startedObserver.ObservedValues.[0].OperationId = completedObserver.ObservedValues.[0].OperationId @>
+        
+    [<Property>]
+    let ``emits started operation when timer started`` () =
+        let operation = Operation()
+        let observer = TestObserver()
+        use subscription = operation.Started.Subscribe(observer)
+        use timer = operation.StartNewTimer()
+        test <@ observer.ObservedValues.Length = 1 @>
+        test <@ not observer.ObservableCompleted @>
+            
+    [<Property>]
+    let ``emits canceled operation when timer canceled`` () =
+        let operation = Operation()
+        let observer = TestObserver()
+        use subscription = operation.Canceled.Subscribe(observer)
+        use timer = operation.StartNewTimer()
+        test <@ observer.ObservedValues = [] @>
+        timer.Cancel()
+        test <@ observer.ObservedValues.Length = 1 @>
+        test <@ not observer.ObservableCompleted @>
+        
+    [<Property>]
+    let ``emits single canceled operation`` () =
+        let operation = Operation()
+        let observer = TestObserver()
+        use subscription = operation.Canceled.Subscribe(observer)
+        use timer = operation.StartNewTimer()
+        test <@ observer.ObservedValues = [] @>
+        timer.Cancel()
+        timer.Cancel()
+        timer.Stop()
+        (timer :> IDisposable).Dispose()
+        test <@ observer.ObservedValues.Length = 1 @>
+        test <@ not observer.ObservableCompleted @>
+        
+    [<Property>]
+    let ``emits completed operation when timer stopped`` () =
+        let operation = Operation()
+        let observer = TestObserver()
+        use subscription = operation.Completed.Subscribe(observer)
+        use timer = operation.StartNewTimer()
+        test <@ observer.ObservedValues = [] @>
+        timer.Stop()
+        test <@ observer.ObservedValues.Length = 1 @>
+        test <@ observer.ObservedValues.[0].Duration > TimeSpan.Zero @>
+        test <@ not observer.ObservableCompleted @>
+        
+    [<Property>]
+    let ``emits completed operation when timer disposed`` () =
+        let operation = Operation()
+        let observer = TestObserver()
+        use subscription = operation.Completed.Subscribe(observer)
+        use timer = operation.StartNewTimer()
+        test <@ observer.ObservedValues = [] @>
+        (timer :> IDisposable).Dispose()
+        test <@ observer.ObservedValues.Length = 1 @>
+        test <@ observer.ObservedValues.[0].Duration > TimeSpan.Zero @>
+        test <@ not observer.ObservableCompleted @>
+        
+    [<Property>]
+    let ``emits single completed operation`` () =
+        let operation = Operation()
+        let observer = TestObserver()
+        use subscription = operation.Completed.Subscribe(observer)
+        use timer = operation.StartNewTimer()
+        test <@ observer.ObservedValues = [] @>
+        timer.Stop()
+        timer.Stop()
+        timer.Cancel()
+        (timer :> IDisposable).Dispose()
+        test <@ observer.ObservedValues.Length = 1 @>
+        test <@ observer.ObservedValues.[0].Duration > TimeSpan.Zero @>
+        test <@ not observer.ObservableCompleted @>
+        
+    [<Property>]
+    let ``times operation with no return value`` () =
+        let operation = Operation()
+        let observer = TestObserver()
+        use subscription = operation.Completed.Subscribe(observer)
+        operation.Time(fun () -> ())
+        test <@ observer.ObservedValues.Length = 1 @>
+        test <@ observer.ObservedValues.[0].Duration > TimeSpan.Zero @>
+        test <@ not observer.ObservableCompleted @>
+        
+    [<Property>]
+    let ``times operation with return value`` (returnValue: obj) =
+        let operation = Operation()
+        let observer = TestObserver()
+        use subscription = operation.Completed.Subscribe(observer)
+        let returnValue' = operation.Time(fun () -> returnValue)
+        test <@ returnValue' = returnValue @>
+        test <@ observer.ObservedValues.Length = 1 @>
+        test <@ observer.ObservedValues.[0].Duration > TimeSpan.Zero @>
+        test <@ not observer.ObservableCompleted @>
+        
+    [<Property>]
+    let ``times async operation with no return value`` () =
+        let operation = Operation()
+        let observer = TestObserver()
+        use subscription = operation.Completed.Subscribe(observer)
+        operation.TimeAsync(fun () -> Task.CompletedTask)
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+        test <@ observer.ObservedValues.Length = 1 @>
+        test <@ observer.ObservedValues.[0].Duration > TimeSpan.Zero @>
+        test <@ not observer.ObservableCompleted @>
+        
+    [<Property>]
+    let ``times async operation with return value`` (returnValue: obj) =
+        let operation = Operation()
+        let observer = TestObserver()
+        use subscription = operation.Completed.Subscribe(observer)
+        let returnValue' =
+            operation.TimeAsync(fun () -> Task.FromResult(returnValue))
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
+        test <@ returnValue' = returnValue @>
+        test <@ observer.ObservedValues.Length = 1 @>
+        test <@ observer.ObservedValues.[0].Duration > TimeSpan.Zero @>
+        test <@ not observer.ObservableCompleted @>
+        
+    [<Property>]
+    let ``times operation with timer context and no return value`` () =
+        let operation = Operation()
+        let observer = TestObserver()
+        use subscription = operation.Completed.Subscribe(observer)
+        operation.Time(fun (timer: Timer) -> ())
+        test <@ observer.ObservedValues.Length = 1 @>
+        test <@ observer.ObservedValues.[0].Duration > TimeSpan.Zero @>
+        test <@ not observer.ObservableCompleted @>
+        
+    [<Property>]
+    let ``times operation with timer context and return value`` (returnValue: obj) =
+        let operation = Operation()
+        let observer = TestObserver()
+        use subscription = operation.Completed.Subscribe(observer)
+        let returnValue' = operation.Time(fun (timer: Timer) -> returnValue)
+        test <@ returnValue' = returnValue @>
+        test <@ observer.ObservedValues.Length = 1 @>
+        test <@ observer.ObservedValues.[0].Duration > TimeSpan.Zero @>
+        test <@ not observer.ObservableCompleted @>
+        
+    [<Property>]
+    let ``times async operation with timer context and no return value`` () =
+        let operation = Operation()
+        let observer = TestObserver()
+        use subscription = operation.Completed.Subscribe(observer)
+        operation.TimeAsync(fun (timer: Timer) -> Task.CompletedTask)
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+        test <@ observer.ObservedValues.Length = 1 @>
+        test <@ observer.ObservedValues.[0].Duration > TimeSpan.Zero @>
+        test <@ not observer.ObservableCompleted @>
+        
+    [<Property>]
+    let ``times async operation with timer context and return value`` (returnValue: obj) =
+        let operation = Operation()
+        let observer = TestObserver()
+        use subscription = operation.Completed.Subscribe(observer)
+        let returnValue' =
+            operation.TimeAsync(fun (timer: Timer) -> Task.FromResult(returnValue))
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
+        test <@ returnValue' = returnValue @>
+        test <@ observer.ObservedValues.Length = 1 @>
+        test <@ observer.ObservedValues.[0].Duration > TimeSpan.Zero @>
+        test <@ not observer.ObservableCompleted @>
+        
