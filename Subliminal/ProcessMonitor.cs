1eb648754e79179e94193ca05dd55c4df5754636
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2019-08-20 21:12:33 +0100

    Add extra sources to process monitor

--- Subliminal/ProcessMonitor.cs
+++ Subliminal/ProcessMonitor.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Reactive.Linq;
 
 namespace Subliminal
 {
     public class ProcessMonitor
     {
-        private ProcessMonitor(IGauge<Process> process)
+        private ProcessMonitor(
+            IGauge<Process> process, ILog<string> standardOutput, ILog<string> standardError, IEvent<ProcessExited> exited)
         {
             Process = process;
+            StandardOutput = standardOutput;
+            StandardError = standardError;
+            Exited = exited;
 
             PrivateMemorySize = process.Select(processSample => processSample.PrivateMemorySize).AsGauge();
             VirtualMemorySize = process.Select(processSample => processSample.VirtualMemorySize).AsGauge();
@@ -26,14 +31,37 @@ namespace Subliminal
 
         public static ProcessMonitor ForProcess(System.Diagnostics.Process process, TimeSpan samplingInterval)
         {
-            process.EnableRaisingEvents = true;
-
             return new ProcessMonitor(
                 process: Observable
                     .Timer(dueTime: DateTimeOffset.UtcNow, period: samplingInterval)
+                    .Do(_ => process.Refresh())
                     .TakeWhile(_ => !process.HasExited)
-                    .Select(_ => GetProcessSnapshot(process))
-                    .AsGauge());
+                    .Select(_ => new Process(
+                        processId: process.Id,
+                        totalProcessorTime: process.TotalProcessorTime,
+                        workingSet: ByteCount.FromBytes(process.WorkingSet64),
+                        privateMemorySize: ByteCount.FromBytes(process.PrivateMemorySize64),
+                        virtualMemorySize: ByteCount.FromBytes(process.VirtualMemorySize64)))
+                    .AsGauge(),
+                standardOutput: Observable
+                    .FromEventPattern<DataReceivedEventHandler, DataReceivedEventArgs>(
+                        handler => process.OutputDataReceived += handler,
+                        handler => process.OutputDataReceived -= handler)
+                    .Select(@event => @event.EventArgs.Data)
+                    .AsLog(),
+                standardError: Observable
+                    .FromEventPattern<DataReceivedEventHandler, DataReceivedEventArgs>(
+                        handler => process.ErrorDataReceived += handler,
+                        handler => process.ErrorDataReceived -= handler)
+                    .Select(@event => @event.EventArgs.Data)
+                    .AsLog(),
+                exited: Observable
+                    .FromEventPattern(
+                        handler => process.Exited += handler,
+                        handler => process.Exited -= handler)
+                    .Do(_ => process.Refresh())
+                    .Select(_ => new ProcessExited(process.Id, process.ExitTime, process.ExitCode))
+                    .AsEvent());
         }
 
         public static ProcessMonitor ForCurrentProcess(TimeSpan samplingInterval)
@@ -41,19 +69,11 @@ namespace Subliminal
             return ForProcess(System.Diagnostics.Process.GetCurrentProcess(), samplingInterval);
         }
 
-        private static Process GetProcessSnapshot(System.Diagnostics.Process process)
-        {
-            process.Refresh();
-
-            return new Process(
-                processId: process.Id,
-                totalProcessorTime: process.TotalProcessorTime,
-                workingSet: ByteCount.FromBytes(process.WorkingSet64),
-                privateMemorySize: ByteCount.FromBytes(process.PrivateMemorySize64),
-                virtualMemorySize: ByteCount.FromBytes(process.VirtualMemorySize64));
-        }
-
         public IGauge<Process> Process { get; }
+        public ILog<string> StandardOutput { get; }
+        public ILog<string> StandardError { get; }
+        public IEvent<ProcessExited> Exited { get; }
+
         public IGauge<ByteCount> PrivateMemorySize { get; }
         public IGauge<ByteCount> VirtualMemorySize { get; }
         public IGauge<ByteCount> WorkingSet { get; }
