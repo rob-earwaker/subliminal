3e00c956ac2e1a6426607444d39bb2dada2d94d4
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2019-08-24 16:24:46 +0100

    Add rate of change tests

--- Subliminal.Tests/TestObservable.fs
+++ Subliminal.Tests/TestObservable.fs
@@ -27,7 +27,7 @@ module ``Test IObservable<TValue>`` =
         test <@ observer.ObservedValues = [ result; result ] @>
         
     [<Property>]
-    let ``rate returns interval between values`` (value1: obj) (value2: obj) (value3: obj) =
+    let ``rate returns intervals between values`` (value1: obj) (value2: obj) (value3: obj) =
         let observable = [ value1; value2; value3 ].ToObservable().Rate()
         let observer = TestObserver()
         use subscription = observable.Subscribe(observer)
@@ -45,6 +45,35 @@ module ``Test IObservable<TValue>`` =
         test <@ observer.ObservedValues.[0].Delta = value1 @>
         test <@ observer.ObservedValues.[1].Delta = value2 @>
         test <@ observer.ObservedValues.[2].Delta = value3 @>
+        
+    [<Property>]
+    let ``rate of change returns intervals between pairwise values`` (value1: obj) (value2: obj) (value3: obj) =
+        let observable = [ value1; value2; value3 ].ToObservable().RateOfChange()
+        let observer = TestObserver()
+        use subscription = observable.Subscribe(observer)
+        test <@ observer.ObservedValues.Length = 2 @>
+        test <@ observer.ObservedValues.[0].Interval > TimeSpan.Zero @>
+        test <@ observer.ObservedValues.[1].Interval > TimeSpan.Zero @>
+        
+    [<Property>]
+    let ``rate of change combines values pairwise`` (value1: obj) (value2: obj) (value3: obj) =
+        let observable = [ value1; value2; value3 ].ToObservable().RateOfChange()
+        let observer = TestObserver()
+        use subscription = observable.Subscribe(observer)
+        test <@ observer.ObservedValues.Length = 2 @>
+        test <@ observer.ObservedValues.[0].Delta.PreviousValue = value1 @>
+        test <@ observer.ObservedValues.[0].Delta.CurrentValue = value2 @>
+        test <@ observer.ObservedValues.[1].Delta.PreviousValue = value2 @>
+        test <@ observer.ObservedValues.[1].Delta.CurrentValue = value3 @>
+        
+    [<Property>]
+    let ``rate of change uses custom selector result`` (value1: obj) (value2: obj) (value3: obj) (result: obj) =
+        let observable = [ value1; value2; value3 ].ToObservable().RateOfChange(fun delta -> result)
+        let observer = TestObserver()
+        use subscription = observable.Subscribe(observer)
+        test <@ observer.ObservedValues.Length = 2 @>
+        test <@ observer.ObservedValues.[0].Delta = result @>
+        test <@ observer.ObservedValues.[1].Delta = result @>
                 
 module ``Test IObservable<int>`` =
     [<Property>]
@@ -55,6 +84,15 @@ module ``Test IObservable<int>`` =
         test <@ observer.ObservedValues.Length = 2 @>
         test <@ observer.ObservedValues.[0] = value2 - value1 @>
         test <@ observer.ObservedValues.[1] = value3 - value2 @>
+    
+    [<Property>]
+    let ``rate of change subtracts pairwise values`` (value1: int) (value2: int) (value3: int) =
+        let observable = [ value1; value2; value3 ].ToObservable().RateOfChange()
+        let observer = TestObserver()
+        use subscription = observable.Subscribe(observer)
+        test <@ observer.ObservedValues.Length = 2 @>
+        test <@ observer.ObservedValues.[0].Delta = value2 - value1 @>
+        test <@ observer.ObservedValues.[1].Delta = value3 - value2 @>
         
 module ``Test IObservable<long>`` =
     [<Property>]
@@ -66,6 +104,15 @@ module ``Test IObservable<long>`` =
         test <@ observer.ObservedValues.[0] = value2 - value1 @>
         test <@ observer.ObservedValues.[1] = value3 - value2 @>
         
+    [<Property>]
+    let ``rate of change subtracts pairwise values`` (value1: int64) (value2: int64) (value3: int64) =
+        let observable = [ value1; value2; value3 ].ToObservable().RateOfChange()
+        let observer = TestObserver()
+        use subscription = observable.Subscribe(observer)
+        test <@ observer.ObservedValues.Length = 2 @>
+        test <@ observer.ObservedValues.[0].Delta = value2 - value1 @>
+        test <@ observer.ObservedValues.[1].Delta = value3 - value2 @>
+        
 module ``Test IObservable<TimeSpan>`` =
     [<Property>]
     let ``delta subtracts pairwise values`` () =
@@ -76,6 +123,22 @@ module ``Test IObservable<TimeSpan>`` =
             test <@ observer.ObservedValues.Length = 2 @>
             test <@ observer.ObservedValues.[0] = value2 - value1 @>
             test <@ observer.ObservedValues.[1] = value3 - value2 @>
+        let arb =
+            Arb.generate<int64>
+            |> Gen.map TimeSpan.FromTicks
+            |> Gen.three
+            |> Arb.fromGen
+        Prop.forAll arb test
+        
+    [<Property>]
+    let ``rate of change subtracts pairwise values`` () =
+        let test (value1: TimeSpan, value2: TimeSpan, value3: TimeSpan) =
+            let observable = [ value1; value2; value3 ].ToObservable().RateOfChange()
+            let observer = TestObserver()
+            use subscription = observable.Subscribe(observer)
+            test <@ observer.ObservedValues.Length = 2 @>
+            test <@ observer.ObservedValues.[0].Delta = value2 - value1 @>
+            test <@ observer.ObservedValues.[1].Delta = value3 - value2 @>
         let arb =
             Arb.generate<int64>
             |> Gen.map TimeSpan.FromTicks
--- Subliminal/IObservableExtensions.cs
+++ Subliminal/IObservableExtensions.cs
@@ -106,6 +106,10 @@ namespace Subliminal
             return observable
                 .TimeInterval()
                 .Buffer(count: 2, skip: 1)
+                // If the observable completes, the last item will be emitted in a buffer containing just
+                // that item. This will have already been emitted as the second value in the previous buffer,
+                // so ignoring this smaller final buffer does not hide the final value from subscribers.
+                .Where(buffer => buffer.Count == 2)
                 .Select(buffer => new Rate<TDelta>(
                     delta: deltaSelector(new Delta<TValue>(previousValue: buffer[0].Value, currentValue: buffer[1].Value)),
                     interval: buffer[1].Interval));
