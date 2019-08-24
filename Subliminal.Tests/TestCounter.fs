24df3d3f2d5e5c6cebc670b1338cc7ff4b64918e
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2019-08-24 12:41:08 +0100

    Add increment extension for counter

--- Subliminal.Tests/TestCounter.fs
+++ Subliminal.Tests/TestCounter.fs
@@ -14,3 +14,23 @@ module ``Test Counter<TIncrement>`` =
         counter.IncrementBy(increment2)
         test <@ observer.ObservedValues = [ increment1; increment2 ] @>
         test <@ not observer.ObservableCompleted @>
+
+module ``Test Counter<int>`` =
+    [<Property>]
+    let ``emits increment of one when incremented`` () =
+        let counter = Counter<int>()
+        let observer = TestObserver()
+        use subscription = counter.Subscribe(observer)
+        counter.Increment()
+        test <@ observer.ObservedValues = [ 1 ] @>
+        test <@ not observer.ObservableCompleted @>
+        
+module ``Test Counter<long>`` =
+    [<Property>]
+    let ``emits increment of one when incremented`` () =
+        let counter = Counter<int64>()
+        let observer = TestObserver()
+        use subscription = counter.Subscribe(observer)
+        counter.Increment()
+        test <@ observer.ObservedValues = [ 1L ] @>
+        test <@ not observer.ObservableCompleted @>
---
+++ Subliminal/CounterExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Subliminal
+{
+    public static class CounterExtensions
+    {
+        public static void Increment(this Counter<int> counter)
+        {
+            counter.IncrementBy(1);
+        }
+
+        public static void Increment(this Counter<long> counter)
+        {
+            counter.IncrementBy(1L);
+        }
+    }
+}
