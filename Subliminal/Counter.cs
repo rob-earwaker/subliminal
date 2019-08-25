63938889d1d6ab41443c94ccc29dc951ded0331f
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2019-08-25 22:06:43 +0100

    Add counter and counterextensions comments

--- Subliminal/Counter.cs
+++ Subliminal/Counter.cs
@@ -2,22 +2,36 @@
 
 namespace Subliminal
 {
+    /// <summary>
+    /// A counter that both captures and emits increments.
+    /// </summary>
     public sealed class Counter<TIncrement> : ICounter<TIncrement>
     {
         private readonly Log<TIncrement> _incrementLog;
         private readonly ICounter<TIncrement> _derivedCounter;
 
+        /// <summary>
+        /// Creates a counter that both captures and emits increments.
+        /// </summary>
         public Counter()
         {
             _incrementLog = new Log<TIncrement>();
             _derivedCounter = _incrementLog.AsCounter();
         }
 
+        /// <summary>
+        /// Captures an increment and emits it to all observers.
+        /// </summary>
         public void IncrementBy(TIncrement increment)
         {
             _incrementLog.Append(increment);
         }
 
+        /// <summary>
+        /// Subscribes an observer such that it receives all future increments emitted
+        /// by the counter. The returned <see cref="IDisposable" /> can be used to
+        /// cancel this subscription.
+        /// </summary>
         public IDisposable Subscribe(IObserver<TIncrement> observer)
         {
             return _derivedCounter.Subscribe(observer);
--- Subliminal/CounterExtensions.cs
+++ Subliminal/CounterExtensions.cs
@@ -1,12 +1,21 @@
 ﻿namespace Subliminal
 {
+    /// <summary>
+    /// Contains extensions for the <see cref="Counter{TIncrement}" /> type.
+    /// </summary>
     public static class CounterExtensions
     {
+        /// <summary>
+        /// Captures an increment of one and emits it to all observers.
+        /// </summary>
         public static void Increment(this Counter<int> counter)
         {
             counter.IncrementBy(1);
         }
 
+        /// <summary>
+        /// Captures an increment of one and emits it to all observers.
+        /// </summary>
         public static void Increment(this Counter<long> counter)
         {
             counter.IncrementBy(1L);
