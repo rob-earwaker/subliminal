a4746ef9a0bb40c7a225b91a22c27a61ef1219e5
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2019-08-27 21:40:13 +0100

    summary docs

--- Subliminal/ICounter.cs
+++ Subliminal/ICounter.cs
@@ -1,5 +1,8 @@
 ﻿namespace Subliminal
 {
+    /// <summary>
+    /// An observable log of increments.
+    /// </summary>
     public interface ICounter<out TIncrement> : ILog<TIncrement>
     {
     }
--- Subliminal/ICounterExtensions.cs
+++ Subliminal/ICounterExtensions.cs
@@ -3,13 +3,24 @@ using System.Reactive.Linq;
 
 namespace Subliminal
 {
+    /// <summary>
+    /// Contains extensions for the <see cref="ICounter{TIncrement}" /> interface.
+    /// </summary>
     public static class ICounterExtensions
     {
+        /// <summary>
+        /// Measures the increment rate of a counter by recording the
+        /// time interval between emitted increments.
+        /// </summary>
         public static IObservable<Rate<TIncrement>> Rate<TIncrement>(this ICounter<TIncrement> counter)
         {
             return counter.Rate(increment => increment);
         }
 
+        /// <summary>
+        /// Measures the increment rate of a counter by recording the
+        /// time interval between emitted increments.
+        /// </summary>
         public static IObservable<Rate<TDelta>> Rate<TIncrement, TDelta>(
             this ICounter<TIncrement> counter, Func<TIncrement, TDelta> incrementSelector)
         {
