4e8462d0f7a37d03684e9cef2e975fbbdfa684eb
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2019-08-27 21:16:14 +0100

    Summary docs

--- Subliminal/DerivedEventLog.cs
+++ Subliminal/DerivedEventLog.cs
@@ -4,6 +4,9 @@ using System.Reactive.Linq;
 
 namespace Subliminal
 {
+    /// <summary>
+    /// An event log that is derived from an observable source.
+    /// </summary>
     public sealed class DerivedEventLog<TEvent> : IEventLog<TEvent>
     {
         private readonly ILog<TEvent> _eventLog;
@@ -13,19 +16,34 @@ namespace Subliminal
             _eventLog = eventLog;
         }
 
+        /// <summary>
+        /// A counter that increments every time an event is raised.
+        /// </summary>
         public ICounter<long> EventCounter => _eventLog.Select(_ => 1L).AsCounter();
 
+        /// <summary>
+        /// Creates an event log from an observable source. This creates a subscription
+        /// to the source observable that will start consuming items immediately.
+        /// </summary>
         public static DerivedEventLog<TEvent> FromObservable(IObservable<TEvent> observable)
         {
             return new DerivedEventLog<TEvent>(observable.AsLog());
         }
 
+        /// <summary>
+        /// Subscribes an observer such that it receives all future events emitted
+        /// by the event log. The returned <see cref="IDisposable" /> can be used to
+        /// cancel this subscription.
+        /// </summary>
         public IDisposable Subscribe(IObserver<TEvent> observer)
         {
             return _eventLog.Subscribe(observer);
         }
     }
 
+    /// <summary>
+    /// An event log that is derived from an observable source.
+    /// </summary>
     public sealed class DerivedEventLog : IEventLog
     {
         private readonly IEventLog<Unit> _eventLog;
@@ -35,13 +53,25 @@ namespace Subliminal
             _eventLog = eventLog;
         }
 
+        /// <summary>
+        /// A counter that increments every time an event is raised.
+        /// </summary>
         public ICounter<long> EventCounter => _eventLog.EventCounter;
 
+        /// <summary>
+        /// Creates an event log from an observable source. This creates a subscription
+        /// to the source observable that will start consuming items immediately.
+        /// </summary>
         public static DerivedEventLog FromObservable(IObservable<Unit> observable)
         {
             return new DerivedEventLog(observable.AsEventLog<Unit>());
         }
 
+        /// <summary>
+        /// Subscribes an observer such that it receives all future events emitted
+        /// by the event log. The returned <see cref="IDisposable" /> can be used to
+        /// cancel this subscription.
+        /// </summary>
         public IDisposable Subscribe(IObserver<Unit> observer)
         {
             return _eventLog.Subscribe(observer);
